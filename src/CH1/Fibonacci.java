7c14cedc5f4d6f9bfecbb7f21f464df08ee3ea90
tsaiyichen <easonjessiejj@gmail.com>
2024-07-11 00:02:17 +0800

    Fibonacci.java

--- src/CH1/Fibonacci.java
+++ src/CH1/Fibonacci.java
@@ -1,11 +1,21 @@
 package CH1;
 
 class Fibonacci {
+    public static int functionCallTime = 0;
+    public static int[] callingTime = new int[11];
+    public static int addTime = 0;
     public static void main(String[] args) {
         System.out.println("Fibonacci numbers 10: " + fibonacci_recursive(10));
+        System.out.println("total function call times: " + functionCallTime);
+        System.out.println("total addtion times: " + addTime);
+        for(int i = 0; i < 11; i++){
+            System.out.printf("function call times of F(%d): %d\n", i, callingTime[i]);
+        }
         System.out.println("Fibonacci numbers 15: " + fibonacci_iteration(15));
     }
     public static int fibonacci_recursive(int n) {
+        functionCallTime++;
+        callingTime[n]++;
         if(n == 0) {
             return 0;
         }
@@ -13,6 +23,7 @@ class Fibonacci {
             return 1;
         }
         else{
+            addTime++;
             return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
         }
     }
