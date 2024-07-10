809809198efd2be8ac1cc4ee3edab0209aff408b
tsaiyichen <easonjessiejj@gmail.com>
2024-07-11 00:07:42 +0800

    Fibonacci.java

--- src/CH1/Fibonacci.java
+++ src/CH1/Fibonacci.java
@@ -1,9 +1,11 @@
 package CH1;
 
 class Fibonacci {
+
     public static int functionCallTime = 0;
     public static int[] callingTime = new int[11];
     public static int addTime = 0;
+
     public static void main(String[] args) {
         System.out.println("Fibonacci numbers 10: " + fibonacci_recursive(10));
         System.out.println("total function call times: " + functionCallTime);
@@ -12,7 +14,9 @@ class Fibonacci {
             System.out.printf("function call times of F(%d): %d\n", i, callingTime[i]);
         }
         System.out.println("Fibonacci numbers 15: " + fibonacci_iteration(15));
+        System.out.println("use dynamic programming: Fib(10): " + dynamicFib(10)[10]);
     }
+
     public static int fibonacci_recursive(int n) {
         functionCallTime++;
         callingTime[n]++;
@@ -27,6 +31,7 @@ class Fibonacci {
             return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
         }
     }
+
     public static int fibonacci_iteration(int n) {
         if(n == 0) {
             return 0;
@@ -41,5 +46,15 @@ class Fibonacci {
             b = c;
         }
         return c;
+    }
+
+    public static int[] dynamicFib(int n){
+        int [] array = new int[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+        for(int i = 2; i <= n; i++){
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        return array;
     }
 }
