c778a31b9f51e9827a77836a2a42af00e3a7dc09
tsaiyichen <easonjessiejj@gmail.com>
2024-07-30 01:12:44 +0800

    Stack

---
+++ src/CH3/MyStack.java
@@ -0,0 +1,51 @@
+package CH3;
+
+public class MyStack {
+    private int top;
+    private int[] stack;
+
+    public MyStack(int n) {
+        top = -1;
+        stack = new int[n];
+    }
+    public boolean isFull(){
+        if(top == stack.length - 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean isEmpty(){
+        if(top == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean push(int x){
+        if(isFull()){
+            System.out.println("Stack is full");
+            return false;
+        }else{
+            System.out.printf("push %d into stack\n", x);
+            stack[++top] = x;
+            return true;
+        }
+    }
+    public int pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return 0;
+        }else{
+            return stack[top--];
+        }
+    }
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return 0;
+        } else {
+            return stack[top];
+        }
+    }
+}
---
+++ src/CH3/ch3Main.java
@@ -0,0 +1,17 @@
+package CH3;
+
+public class ch3Main {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
